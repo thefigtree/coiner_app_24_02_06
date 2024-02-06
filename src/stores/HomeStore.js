2853dd9f85cdb9633575093b2e7e81e21c7f5025
thefigtree <ldj8431@naver.com>
2024-02-06 23:32:12 +0900

    Coins Array Data

--- src/stores/HomeStore.js
+++ src/stores/HomeStore.js
@@ -2,10 +2,22 @@ import axios from "axios";
 import { create } from "zustand";
 
 export const HomeStore = create((set) => ({
+  coins: [],
+
   fetchCoins: async () => {
     const res = await axios.get(
       "https://api.coingecko.com/api/v3/search/trending"
     );
-    console.log(res);
+
+    const coins = res.data.coins.map((coin) => {
+      return {
+        name: coin.item.name,
+        image: coin.item.large,
+        id: coin.item.id,
+        priceBtc: coin.item.price_btc,
+      };
+    });
+
+    set({ coins });
   },
 }));
