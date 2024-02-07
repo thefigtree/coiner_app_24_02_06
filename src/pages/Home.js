6b46d62a84b04d310fe9b4881ee51a01db93bc32
thefigtree <ldj8431@naver.com>
2024-02-07 18:38:11 +0900

    Bitcoin Data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { HomeStore } from "../stores/HomeStore";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
   const store = HomeStore();
@@ -8,5 +9,16 @@ export const Home = () => {
     store.fetchCoins();
   }, []);
 
-  return <div>Home</div>;
+  return (
+    <div>
+      <input type="text" value={store.query} onChange={store.setQuery}></input>
+      {store.coins.map((coin) => {
+        return (
+          <div key={coin.id}>
+            <Link to={`/${coin.id}`}>{coin.name}</Link>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
--- src/stores/HomeStore.js
+++ src/stores/HomeStore.js
@@ -3,6 +3,21 @@ import { create } from "zustand";
 
 export const HomeStore = create((set) => ({
   coins: [],
+  query: "",
+
+  setQuery: (e) => {
+    set({ query: e.target.value });
+    HomeStore.getState().searchCoins();
+  },
+
+  searchCoins: async () => {
+    const { query } = HomeStore.getState();
+    // console.log(query);
+    const res = await axios.get(
+      `https://api.coingecko.com/api/v3/search?query=${query}`
+    );
+    console.log(res);
+  },
 
   fetchCoins: async () => {
     const res = await axios.get(
