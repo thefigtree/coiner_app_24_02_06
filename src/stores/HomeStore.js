69c806f2c0a900c7f710a0cc76f6827196ee08bc
thefigtree <ldj8431@naver.com>
2024-02-09 20:09:03 +0900

    HomeStore priceUsd

--- src/stores/HomeStore.js
+++ src/stores/HomeStore.js
@@ -25,6 +25,7 @@ export const HomeStore = create((set) => ({
           name: coin.name,
           image: coin.large,
           id: coin.id,
+          priceBtc: coin.item.price_btc,
         };
       });
       set({ coins });
@@ -34,18 +35,30 @@ export const HomeStore = create((set) => ({
   }, 500),
 
   fetchCoins: async () => {
-    const res = await axios.get(
-      "https://api.coingecko.com/api/v3/search/trending"
-    );
+    const [res, btcRes] = await Promise.all([
+      await axios.get("https://api.coingecko.com/api/v3/search/trending"),
+      axios.get(
+        "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+      ),
+    ]);
+
+    const btcPrice = btcRes.data.bitcoin.usd;
+    // console.log(btcPrice);
+
+    // const res = await axios.get(
+    //   "https://api.coingecko.com/api/v3/search/trending"
+    // );
 
     const coins = res.data.coins.map((coin) => {
       return {
         name: coin.item.name,
         image: coin.item.large,
         id: coin.item.id,
-        priceBtc: coin.item.price_btc,
+        priceBtc: coin.item.price_btc.toFixed(10),
+        priceUsd: (coin.item.price_btc * btcPrice).toFixed(10),
       };
     });
+    console.log(coins);
 
     set({ coins, trending: coins });
   },
