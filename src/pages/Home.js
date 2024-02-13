74e6b1c704a0fc50c6b5237cc6e16ebbfd4ec1d2
thefigtree <ldj8431@naver.com>
2024-02-14 00:09:05 +0900

    Home Title

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,16 @@ import { Link } from "react-router-dom";
 import { Header } from "../components/Header";
 import styled from "styled-components";
 
-const Title = styled.h2``;
+const Title = styled.h2`
+  padding: 40px 0;
+  text-align: center;
+  /* background-color: aliceblue; */
+  h2 {
+    font-family: "Gowun Dodum", sans-serif;
+    font-size: 36px;
+    color: white;
+  }
+`;
 
 export const Home = () => {
   const store = HomeStore();
@@ -17,6 +26,7 @@ export const Home = () => {
     <div>
       <Header></Header>
       <Title>
+        <h2>Search For A Coin</h2>
         <input
           type="text"
           value={store.query}
