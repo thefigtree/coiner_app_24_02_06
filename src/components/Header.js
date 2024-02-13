eb351d459e8a21c3a3ad91bf78502b0d98a26a3e
thefigtree <ldj8431@naver.com>
2024-02-13 23:25:53 +0900

    Header Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,28 @@
 import styled from "styled-components";
+import { routes } from "../routes";
+import { Link } from "react-router-dom";
 
-const SHeader = styled.header``;
+const SHeader = styled.header`
+  width: 100%;
+  padding: 20px 5%;
+  background-color: rebeccapurple;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Logo = styled.div`
+  font-size: 24px;
+  font-weight: 700;
+  /* background-color: aliceblue; */
+`;
 
 export const Header = () => {
-  return <SHeader>Header</SHeader>;
+  return (
+    <SHeader>
+      <Logo>
+        <Link to={routes.home}>TRENDING CRYPTOs</Link>
+      </Logo>
+    </SHeader>
+  );
 };
